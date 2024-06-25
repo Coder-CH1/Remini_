31845e0a8bfff2e58d55c7fa9e2091c2771091e8
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-06-25 01:59:34 +0100

    added

---
+++ Remini_/OpenCVHeader.h
@@ -0,0 +1,12 @@
+//
+//  OpenCVHeader.h
+//  Remini_
+//
+//  Created by Mac on 25/06/2024.
+//
+
+#ifndef OpenCVHeader_h
+#define OpenCVHeader_h
+
+
+#endif /* OpenCVHeader_h */
---
+++ Remini_/OpenCVWrapper.mm
@@ -0,0 +1,8 @@
+//
+//  OpenCVWrapper.m
+//  Remini_
+//
+//  Created by Mac on 25/06/2024.
+//
+
+#import <Foundation/Foundation.h>
