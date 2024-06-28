05195fb846f743837049ea974c144d468fcc4a87
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-06-28 14:48:35 +0100

    added

--- Remini_/OpenCVWrapper.mm
+++ Remini_/OpenCVWrapper.mm
@@ -5,30 +5,41 @@
 //  Created by Mac on 25/06/2024.
 //
 
+// OpenCVWrapper.m
+
 #import "OpenCVWrapper.h"
-#import <UIKit/UIKit.h>
-#import <CoreML/CoreML.h>
-#import <CoreImage/CoreImage.h>
-#import <CoreGraphics/CoreGraphics.h>
-#import <CoreFoundation/CoreFoundation.h>
-#import <opencv2/opencv.hpp>
+//#import <UIKit/UIKit.h>
 
 @implementation OpenCVWrapper
 
 + (UIImage *)processImage:(UIImage *)inputImage {
-    cv::Mat cvImage;
-    cvImage = [self cvMatFromUIImage:inputImage];
-    cv::cvtColor(cvImage, cvImage, cv::COLOR_COLORCVT_MAX);
-    UIImage *outputImage = [self UIImageFromCVMat:cvImage];
-    return outputImage;
+cv::Mat cvImage = [self cvMatFromUIImage:inputImage];
+cv::cvtColor(cvImage, cvImage, cv::COLOR_BGR2GRAY); // Convert to grayscale
+UIImage *outputImage = [self UIImageFromCVMat:cvImage];
+return outputImage;
 }
 
 + (cv::Mat)cvMatFromUIImage:(UIImage *)image {
-    return  cv::Mat();
+CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
+CGFloat cols = image.size.width;
+CGFloat rows = image.size.height;
+cv::Mat cvImage(rows, cols, CV_8UC4); // 8-bit, 4-channel (RGBA)
+CGContextRef contextRef = CGBitmapContextCreate(cvImage.data, cols, rows, 8, cvImage.step[0], colorSpace, kCGImageAlphaNoneSkipLast | kCGBitmapByteOrderDefault);
+CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
+CGContextRelease(contextRef);
+return cvImage;
 }
 
 + (UIImage *)UIImageFromCVMat:(cv::Mat)cvMat {
-    return  [UIImage new];
+NSData *data = [NSData dataWithBytes:cvMat.data length:cvMat.elemSize() * cvMat.total()];
+CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef)data);
+CGImageRef imageRef = CGImageCreate(cvMat.cols, cvMat.rows, 8, 8 * 4, cvMat.step[0], colorSpace, kCGImageAlphaNoneSkipLast | kCGBitmapByteOrderDefault, provider, NULL, false, kCGRenderingIntentDefault);
+UIImage *outputImage = [UIImage imageWithCGImage:imageRef];
+CGImageRelease(imageRef);
+CGDataProviderRelease(provider);
+CGColorSpaceRelease(colorSpace);
+return outputImage;
 }
 
 @end
